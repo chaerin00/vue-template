974fb8bda3b6561080aa019f9b818d5194a234d1
chaerin <achrvv@sookmyung.ac.kr>
2021-10-16 15:59:47 +0900

    feat: set project

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="hello">
+    <h1 class="hello-text">{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Vue.js 시작하기'
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.hello {
+  &-text {
+    color: green;
+  }
+}
+</style>
